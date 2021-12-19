6cbc46f0560ad0f2fb89094a080283628b8c917e
Jin Choi <jin.choi@cern.ch>
2021-12-19 23:07:09 +0900

    update efficiencies

---
+++ trigEmuCuts/CanvasConfigs.py
@@ -0,0 +1,255 @@
+# Configs for Canvas.ipynb
+configs = {
+    "passEle12/electron/Barrel/Full5x5_sigmaIetaIeta": {
+        "x_title": "Full 5x5 #sigma_{i#etai#eta}",
+        "x_range": [0., 0.08],
+        "y_title": "N_{evt}",
+        "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
+        "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle12/electron/EndCap/Full5x5_sigmaIetaIeta": {
+        "x_title": "Full 5x5 #sigma_{i#etai#eta}",
+        "x_range": [0., 0.08],
+        "y_title": "N_{evt}",
+        "extra_info2": "Endcap, 1.479 < |#eta_{sc}| < 2.5",
+        "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle23/electron/Barrel/Full5x5_sigmaIetaIeta": {
+        "x_title": "Full 5x5 #sigma_{i#etai#eta}",
+        "x_range": [0., 0.08],
+        "y_title": "N_{evt}",
+        "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
+        "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle23/electron/EndCap/Full5x5_sigmaIetaIeta": {
+        "x_title": "Full 5x5 #sigma_{i#etai#eta}",
+        "x_range": [0., 0.08],
+        "y_title": "N_{evt}",
+        "extra_info2": "Endcap, 1.479 < |#eta_{sc}| < 2.5",
+        "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle12/electron/Barrel/HoverE": {
+        "x_title": "H/E",
+        "x_range": [0., 0.5],
+        "y_title": "N_{evt}",
+        "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
+        "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle12/electron/EndCap/HoverE": {
+        "x_title": "H/E",
+        "x_range": [0., 0.5],
+        "y_title": "N_{evt}",
+        "extra_info2": "Endcap, 1.479 < |#eta_{sc}| < 2.5",
+        "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle23/electron/Barrel/HoverE": {
+        "x_title": "H/E",
+        "x_range": [0., 0.5],
+        "y_title": "N_{evt}",
+        "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
+        "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle23/electron/EndCap/HoverE": {
+        "x_title": "H/E",
+        "x_range": [0., 0.5],
+        "y_title": "N_{evt}",
+        "extra_info2": "Endcap, 1.479 < |#eta_{sc}| < 2.5",
+        "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle12/electron/Barrel/ecalPFClusterIso": {
+        "x_title": "ECAL PFIso/P_{T}",
+        "x_range": [0., 0.5],
+        "y_title": "N_{evt}",
+        "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
+        "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle12/electron/EndCap/ecalPFClusterIso": {
+        "x_title": "ECAL PFIso/P_{T}",
+        "x_range": [0., 0.5],
+        "y_title": "N_{evt}",
+        "extra_info2": "Endcap, 1.479 < |#eta_{sc}| < 2.5",
+        "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle23/electron/Barrel/ecalPFClusterIso": {
+        "x_title": "ECAL PFIso/P_{T}",
+        "x_range": [0., 0.5],
+        "y_title": "N_{evt}",
+        "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
+        "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle23/electron/EndCap/ecalPFClusterIso": {
+        "x_title": "ECAL PFIso/P_{T}",
+        "x_range": [0., 0.5],
+        "y_title": "N_{evt}",
+        "extra_info2": "Endcap, 1.479 < |#eta_{sc}| < 2.5",
+        "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle12/electron/Barrel/ecalPFClusterIso_rho": {
+        "x_title": "(ECAL PFIso/P_{T}-0.5)/#rho",
+        "x_range": [0., 0.5],
+        "y_title": "N_{evt}",
+        "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
+        "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle12/electron/EndCap/ecalPFClusterIso_rho": {
+        "x_title": "(ECAL PFIso/P_{T}-0.5)/#rho",
+        "x_range": [0., 0.5],
+        "y_title": "N_{evt}",
+        "extra_info2": "Endcap, 1.479 < |#eta_{sc}| < 2.5",
+        "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle23/electron/Barrel/ecalPFClusterIso_rho": {
+        "x_title": "(ECAL PFIso/P_{T}-0.5)/#rho",
+        "x_range": [0., 1.],
+        "y_title": "N_{evt}",
+        "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
+        "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle23/electron/EndCap/ecalPFClusterIso_rho": {
+        "x_title": "(ECAL PFIso/P_{T}-0.5)/#rho",
+        "x_range": [0., 1.],
+        "y_title": "N_{evt}",
+        "extra_info2": "Endcap, 1.479 < |#eta_{sc}| < 2.5",
+        "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle12/electron/Barrel/hcalPFClusterIso": {
+        "x_title": "HCAL PFIso/P_{T}",
+        "x_range": [0., 0.5],
+        "y_title": "N_{evt}",
+        "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
+        "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle12/electron/EndCap/hcalPFClusterIso": {
+        "x_title": "HCAL PFIso/P_{T}",
+        "x_range": [0., 0.5],
+        "y_title": "N_{evt}",
+        "extra_info2": "Endcap, 1.479 < |#eta_{sc}| < 2.5",
+        "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle23/electron/Barrel/hcalPFClusterIso": {
+        "x_title": "HCAL PFIso/P_{T}",
+        "x_range": [0., 0.5],
+        "y_title": "N_{evt}",
+        "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
+        "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle23/electron/EndCap/hcalPFClusterIso": {
+        "x_title": "HCAL PFIso/P_{T}",
+        "x_range": [0., 0.5],
+        "y_title": "N_{evt}",
+        "extra_info2": "Endcap, 1.479 < |#eta_{sc}| < 2.5",
+        "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle12/electron/Barrel/hcalPfClusterIso_rho": {
+        "x_title": "(HCAL PFIso/P_{T}-0.5)/#rho",
+        "x_range": [0., 0.5],
+        "y_title": "N_{evt}",
+        "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
+        "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle12/electron/EndCap/hcalPfClusterIso_rho": {
+        "x_title": "(HCAL PFIso/P_{T}-0.5)/#rho",
+        "x_range": [0., 0.5],
+        "y_title": "N_{evt}",
+        "extra_info2": "Endcap, 1.479 < |#eta_{sc}| < 2.5",
+        "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle23/electron/Barrel/hcalPfClusterIso_rho": {
+        "x_title": "(HCAL PFIso/P_{T}-0.5)/#rho",
+        "x_range": [0., 1.],
+        "y_title": "N_{evt}",
+        "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
+        "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle23/electron/EndCap/hcalPfClusterIso_rho": {
+        "x_title": "(HCAL PFIso/P_{T}-0.5)/#rho",
+        "x_range": [0., 1.],
+        "y_title": "N_{evt}",
+        "extra_info2": "Endcap, 1.479 < |#eta_{sc}| < 2.5",
+        "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle12/electron/Barrel/dEtaSeed": {
+        "x_title": "|#Delta#eta_{seed}|",
+        "x_range": [0., 0.12],
+        "y_title": "N_{evt}",
+        "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
+        "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle12/electron/EndCap/dEtaSeed": {
+        "x_title": "|#Delta#eta_{seed}|",
+        "x_range": [0., 0.12],
+        "y_title": "N_{evt}",
+        "extra_info2": "Endcap, 1.479 < |#eta_{sc}| < 2.5",
+        "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle23/electron/Barrel/dEtaSeed": {
+        "x_title": "|#Delta#eta_{seed}|",
+        "x_range": [0., 0.12],
+        "y_title": "N_{evt}",
+        "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
+        "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle23/electron/EndCap/dEtaSeed": {
+        "x_title": "|#Delta#eta_{seed}",
+        "x_range": [0., 0.12],
+        "y_title": "N_{evt}",
+        "extra_info2": "Endcap, 1.479 < |#eta_{sc}| < 2.5",
+        "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle12/electron/Barrel/dPhiIn": {
+        "x_title": "|#Delta#phi_{In}|",
+        "x_range": [0., 0.7],
+        "y_title": "N_{evt}",
+        "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
+        "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle12/electron/EndCap/dPhiIn": {
+        "x_title": "|#Delta#phi_{In}|",
+        "x_range": [0., 0.7],
+        "y_title": "N_{evt}",
+        "extra_info2": "Endcap, 1.479 < |#eta_{sc}| < 2.5",
+        "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle23/electron/Barrel/dPhiIn": {
+        "x_title": "|#Delta#phi_{In}|",
+        "x_range": [0., 0.7],
+        "y_title": "N_{evt}",
+        "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
+        "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle23/electron/EndCap/dPhiIn": {
+        "x_title": "|#Delta#phi_{In}|",
+        "x_range": [0., 0.7],
+        "y_title": "N_{evt}",
+        "extra_info2": "Endcap, 1.479 < |#eta_{sc}| < 2.5",
+        "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle12/electron/Barrel/dr03TkSumPt": {
+        "x_title": "#DeltaR03TrkSumP_{T} / P_{T}",
+        "x_range": [0., 1.],
+        "y_title": "N_{evt}",
+        "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
+        "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle12/electron/EndCap/dr03TkSumPt": {
+        "x_title": "#DeltaR03TrkSumP_{T} / P_{T}",
+        "x_range": [0., 1.],
+        "y_title": "N_{evt}",
+        "extra_info2": "Endcap, 1.479 < |#eta_{sc}| < 2.5",
+        "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle23/electron/Barrel/dr03TkSumPt": {
+        "x_title": "#DeltaR03TrkSumP_{T} / P_{T}",
+        "x_range": [0., 1.],
+        "y_title": "N_{evt}",
+        "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
+        "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
+    },
+    "passEle23/electron/EndCap/dr03TkSumPt": {
+        "x_title": "#DeltaR03TrkSumP_{T} / P_{T}",
+        "x_range": [0., 1.],
+        "y_title": "N_{evt}",
+        "extra_info2": "Endcap, 1.479 < |#eta_{sc}| < 2.5",
+        "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
+    },
+}
