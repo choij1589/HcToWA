3de5342b4486d55a85f3a0a8a23f0d4c84846e80
Jin Choi <jin.choi@cern.ch>
2022-01-06 16:29:45 +0900

    update EFF validation

--- trigEmuCuts/CanvasConfigs.py
+++ trigEmuCuts/CanvasConfigs.py
@@ -7,7 +7,7 @@ configs = {
         "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
         "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
     },
-    "passEle12/electron/EndCap/Full5x5_sigmaIetaIeta": {
+    "passEle12/electron/Endcap/Full5x5_sigmaIetaIeta": {
         "x_title": "Full 5x5 #sigma_{i#etai#eta}",
         "x_range": [0., 0.08],
         "y_title": "N_{evt}",
@@ -21,7 +21,7 @@ configs = {
         "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
         "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
     },
-    "passEle23/electron/EndCap/Full5x5_sigmaIetaIeta": {
+    "passEle23/electron/Endcap/Full5x5_sigmaIetaIeta": {
         "x_title": "Full 5x5 #sigma_{i#etai#eta}",
         "x_range": [0., 0.08],
         "y_title": "N_{evt}",
@@ -35,7 +35,7 @@ configs = {
         "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
         "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
     },
-    "passEle12/electron/EndCap/HoverE": {
+    "passEle12/electron/Endcap/HoverE": {
         "x_title": "H/E",
         "x_range": [0., 0.5],
         "y_title": "N_{evt}",
@@ -49,7 +49,7 @@ configs = {
         "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
         "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
     },
-    "passEle23/electron/EndCap/HoverE": {
+    "passEle23/electron/Endcap/HoverE": {
         "x_title": "H/E",
         "x_range": [0., 0.5],
         "y_title": "N_{evt}",
@@ -63,7 +63,7 @@ configs = {
         "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
         "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
     },
-    "passEle12/electron/EndCap/ecalPFClusterIso": {
+    "passEle12/electron/Endcap/ecalPFClusterIso": {
         "x_title": "ECAL PFIso/P_{T}",
         "x_range": [0., 0.5],
         "y_title": "N_{evt}",
@@ -77,7 +77,7 @@ configs = {
         "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
         "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
     },
-    "passEle23/electron/EndCap/ecalPFClusterIso": {
+    "passEle23/electron/Endcap/ecalPFClusterIso": {
         "x_title": "ECAL PFIso/P_{T}",
         "x_range": [0., 0.5],
         "y_title": "N_{evt}",
@@ -86,28 +86,28 @@ configs = {
     },
     "passEle12/electron/Barrel/ecalPFClusterIso_rho": {
         "x_title": "(ECAL PFIso/P_{T}-0.5)/#rho",
-        "x_range": [0., 0.5],
+        "x_range": [-0.2, 0.3],
         "y_title": "N_{evt}",
         "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
         "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
     },
-    "passEle12/electron/EndCap/ecalPFClusterIso_rho": {
+    "passEle12/electron/Endcap/ecalPFClusterIso_rho": {
         "x_title": "(ECAL PFIso/P_{T}-0.5)/#rho",
-        "x_range": [0., 0.5],
+        "x_range": [-0.2, 0.3],
         "y_title": "N_{evt}",
         "extra_info2": "Endcap, 1.479 < |#eta_{sc}| < 2.5",
         "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
     },
     "passEle23/electron/Barrel/ecalPFClusterIso_rho": {
         "x_title": "(ECAL PFIso/P_{T}-0.5)/#rho",
-        "x_range": [0., 1.],
+        "x_range": [-0.2, 0.3],
         "y_title": "N_{evt}",
         "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
         "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
     },
-    "passEle23/electron/EndCap/ecalPFClusterIso_rho": {
+    "passEle23/electron/Endcap/ecalPFClusterIso_rho": {
         "x_title": "(ECAL PFIso/P_{T}-0.5)/#rho",
-        "x_range": [0., 1.],
+        "x_range": [-0.2, 0.3],
         "y_title": "N_{evt}",
         "extra_info2": "Endcap, 1.479 < |#eta_{sc}| < 2.5",
         "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
@@ -119,9 +119,9 @@ configs = {
         "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
         "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
     },
-    "passEle12/electron/EndCap/hcalPFClusterIso": {
+    "passEle12/electron/Endcap/hcalPFClusterIso": {
         "x_title": "HCAL PFIso/P_{T}",
-        "x_range": [0., 0.5],
+        "x_range": [0, 0.5],
         "y_title": "N_{evt}",
         "extra_info2": "Endcap, 1.479 < |#eta_{sc}| < 2.5",
         "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
@@ -133,37 +133,37 @@ configs = {
         "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
         "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
     },
-    "passEle23/electron/EndCap/hcalPFClusterIso": {
+    "passEle23/electron/Endcap/hcalPFClusterIso": {
         "x_title": "HCAL PFIso/P_{T}",
         "x_range": [0., 0.5],
         "y_title": "N_{evt}",
         "extra_info2": "Endcap, 1.479 < |#eta_{sc}| < 2.5",
         "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
     },
-    "passEle12/electron/Barrel/hcalPfClusterIso_rho": {
+    "passEle12/electron/Barrel/hcalPFClusterIso_rho": {
         "x_title": "(HCAL PFIso/P_{T}-0.5)/#rho",
-        "x_range": [0., 0.5],
+        "x_range": [-0.2, 0.3],
         "y_title": "N_{evt}",
         "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
         "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
     },
-    "passEle12/electron/EndCap/hcalPfClusterIso_rho": {
+    "passEle12/electron/Endcap/hcalPFClusterIso_rho": {
         "x_title": "(HCAL PFIso/P_{T}-0.5)/#rho",
-        "x_range": [0., 0.5],
+        "x_range": [-0.2, 0.3],
         "y_title": "N_{evt}",
         "extra_info2": "Endcap, 1.479 < |#eta_{sc}| < 2.5",
         "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
     },
-    "passEle23/electron/Barrel/hcalPfClusterIso_rho": {
+    "passEle23/electron/Barrel/hcalPFClusterIso_rho": {
         "x_title": "(HCAL PFIso/P_{T}-0.5)/#rho",
-        "x_range": [0., 1.],
+        "x_range": [-0.2, 0.3],
         "y_title": "N_{evt}",
         "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
         "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
     },
-    "passEle23/electron/EndCap/hcalPfClusterIso_rho": {
+    "passEle23/electron/Endcap/hcalPFClusterIso_rho": {
         "x_title": "(HCAL PFIso/P_{T}-0.5)/#rho",
-        "x_range": [0., 1.],
+        "x_range": [-0.2, 0.3],
         "y_title": "N_{evt}",
         "extra_info2": "Endcap, 1.479 < |#eta_{sc}| < 2.5",
         "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
@@ -175,7 +175,7 @@ configs = {
         "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
         "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
     },
-    "passEle12/electron/EndCap/dEtaSeed": {
+    "passEle12/electron/Endcap/dEtaSeed": {
         "x_title": "|#Delta#eta_{seed}|",
         "x_range": [0., 0.12],
         "y_title": "N_{evt}",
@@ -189,7 +189,7 @@ configs = {
         "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
         "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
     },
-    "passEle23/electron/EndCap/dEtaSeed": {
+    "passEle23/electron/Endcap/dEtaSeed": {
         "x_title": "|#Delta#eta_{seed}",
         "x_range": [0., 0.12],
         "y_title": "N_{evt}",
@@ -203,7 +203,7 @@ configs = {
         "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
         "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
     },
-    "passEle12/electron/EndCap/dPhiIn": {
+    "passEle12/electron/Endcap/dPhiIn": {
         "x_title": "|#Delta#phi_{In}|",
         "x_range": [0., 0.7],
         "y_title": "N_{evt}",
@@ -217,7 +217,7 @@ configs = {
         "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
         "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
     },
-    "passEle23/electron/EndCap/dPhiIn": {
+    "passEle23/electron/Endcap/dPhiIn": {
         "x_title": "|#Delta#phi_{In}|",
         "x_range": [0., 0.7],
         "y_title": "N_{evt}",
@@ -231,7 +231,7 @@ configs = {
         "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
         "extra_info1": "Ele12_CaloIdL_TrackIdL_IsoVL"
     },
-    "passEle12/electron/EndCap/dr03TkSumPt": {
+    "passEle12/electron/Endcap/dr03TkSumPt": {
         "x_title": "#DeltaR03TrkSumP_{T} / P_{T}",
         "x_range": [0., 1.],
         "y_title": "N_{evt}",
@@ -245,7 +245,7 @@ configs = {
         "extra_info2": "Barrel, |#eta_{sc}| < 1.479",
         "extra_info1": "Ele23_CaloIdL_TrackIdL_IsoVL"
     },
-    "passEle23/electron/EndCap/dr03TkSumPt": {
+    "passEle23/electron/Endcap/dr03TkSumPt": {
         "x_title": "#DeltaR03TrkSumP_{T} / P_{T}",
         "x_range": [0., 1.],
         "y_title": "N_{evt}",
