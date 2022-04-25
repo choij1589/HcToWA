69e961ea29000b8e1a72fba1ab38160df0c9ac91
Jin Choi <jin.choi@cern.ch>
2022-04-25 19:48:07 +0900

    regular update 220425

---
+++ MetaInfo/HcToWAElectron-FullRangeEta/2016postVFP_HcToWATight/fits_sim.d/s6m0/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd $TNP_BASE
+python tnp_tamsa.py config/HcToWAElectron.py 2016postVFP_HcToWATight --step fit --set 6 --member 0 --sim --bin $1 --no-condor
+exit $?
--- SignalRegion/EventToGraph.py
+++
@@ -1,6 +0,0 @@
-import torch
-from torch_geometric.data import Data
-from ROOT import TFile
-
-
-
